566107ed93436a2782bfb77db5399fa8190b993c
Vasil Nikolov <phpnikolov@gmail.com>
2023-02-04 18:45:19 +0200

    Added function to update leaser config

--- src/menu/leaser.ts
+++ src/menu/leaser.ts
@@ -1,5 +1,7 @@
-import { Leaser } from "@nolus/nolusjs/build/contracts";
+import { NolusWallet } from "@nolus/nolusjs";
+import { Leaser, LeaserConfig } from "@nolus/nolusjs/build/contracts";
 import { NolusHelper } from "../NolusHelper";
+import { MenuUtil } from "./util";
 const inquirer = require('inquirer');
 
 class MenuLeaser {
@@ -17,7 +19,6 @@ class MenuLeaser {
     }
 
     async show() {
-        let leaser: Leaser = await this.getLeaser();
         while (true) {
             console.log();
             const { menuChoice } = await inquirer.prompt([
@@ -37,7 +38,60 @@ class MenuLeaser {
                 return;
             }
 
-            console.log("not implemented");
+            if (menuChoice === 'config') {
+                await this.showConfig();
+            }
+        }
+    }
+
+    async showConfig() {
+        const leaser: Leaser = await this.getLeaser();
+
+
+        while (true) {
+            console.log();
+            const { menuChoice } = await inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'menuChoice',
+                    message: '[Leaser -> Config]',
+                    choices: [
+                        { name: 'Show', value: 'show' },
+                        { name: 'Update', value: 'update' },
+                        new inquirer.Separator(),
+                        { name: 'Back', value: 'back' }
+                    ]
+                }
+            ]);
+
+            if (menuChoice === 'back') {
+                return;
+            }
+
+            if (menuChoice === 'show') {
+                console.log(await leaser.getLeaserConfig());
+            }
+            else if (menuChoice === 'update') {
+                const contractsOwnerWallet: NolusWallet = await NolusHelper.getWallet(NolusHelper.config.keys.contracts_owner);
+                let leaserConfig: LeaserConfig;
+
+                while (true) {
+                    let config: string = await MenuUtil.askString("Enter JSON config or type \"back\"");
+                    if (config.toLowerCase() === "back") {
+                        break;
+                    }
+                    try {
+                        leaserConfig = JSON.parse(config);
+                        await leaser.setLeaserConfig(contractsOwnerWallet, leaserConfig, "auto")
+                            .then(() => console.log(`Config is updated successfully`))
+                            .catch(console.error);
+                        break;
+                    }
+                    catch (error) {
+                        console.error("Invalid JSON: " + error)
+                    }
+                }
+            }
         }
     }
 }
