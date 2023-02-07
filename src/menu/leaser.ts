2e41fe1a9a628e63d1176c10cd1fb79db9aebf01
Vasil Nikolov <phpnikolov@gmail.com>
2023-02-07 18:06:17 +0200

    Add more functionality

--- src/menu/leaser.ts
+++ src/menu/leaser.ts
@@ -1,6 +1,6 @@
 import { NolusWallet } from "@nolus/nolusjs";
 import { Leaser, LeaserConfig } from "@nolus/nolusjs/build/contracts";
-import { NolusHelper } from "../NolusHelper";
+import { Coin, NolusHelper } from "../NolusHelper";
 import { MenuUtil } from "./util";
 const inquirer = require('inquirer');
 
@@ -19,6 +19,7 @@ class MenuLeaser {
     }
 
     async show() {
+        const leaser: Leaser = await this.getLeaser();
         while (true) {
             console.log();
             const { menuChoice } = await inquirer.prompt([
@@ -28,6 +29,8 @@ class MenuLeaser {
                     message: `[Leaser]`,
                     choices: [
                         { name: 'Config', value: 'config' },
+                        { name: 'Open Lease', value: 'openLease' },
+                        { name: 'Show Open Leases', value: 'showOpenLeases' },
                         new inquirer.Separator(),
                         { name: 'Back', value: 'back' }
                     ]
@@ -40,6 +43,70 @@ class MenuLeaser {
 
             if (menuChoice === 'config') {
                 await this.showConfig();
+            }
+            else if (menuChoice === 'openLease') {
+                const wallet: NolusWallet = await NolusHelper.promptAccount();
+
+                const coins: Coin[] = NolusHelper.getCoins();
+                const amounts = await NolusHelper.getCoinsAmounts(wallet.address, coins);
+                let choices = [];
+                for (let i in amounts) {
+                    const el = amounts[i];
+
+                    if (el.amount > 0) {
+                        choices.push({ name: el.coin.symbol + ` (max ${NolusHelper.amountFormatter.format(el.amount / Math.pow(10, el.coin.decimal_digits))})`, value: el });
+                    }
+
+                }
+                console.log();
+                let { downpayment } = await inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'coinChoice',
+                        message: 'Select conin for the Down Payment',
+                        choices: [...choices, new inquirer.Separator(), 'Back']
+                    }
+                ]);
+                if (downpayment === 'Back') {
+                    return;
+                }
+
+                const { amount } = await inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'amount',
+                        message: `Enter the amount of ${downpayment.coin.symbol} for the Down Payment (max ${NolusHelper.amountFormatter.format(downpayment.amount / Math.pow(10, downpayment.coin.decimal_digits))})`,
+                    }
+                ]);
+
+                leaser.openLease(wallet, "WBTC", "auto", [{
+                    denom: downpayment.coin.denom,
+                    amount: (parseInt(amount) * Math.pow(10, downpayment.coin.decimal_digits)).toFixed(0)
+                }])
+            }
+            else if (menuChoice === 'showOpenLeases') {
+                let addresses = [];
+                for (let accountName in NolusHelper.config.keys) {
+                    let accAddress = (await NolusHelper.getWallet(NolusHelper.config.keys[accountName])).address;
+                    addresses.push({ name: accountName, value: accAddress });
+                }
+
+                console.log();
+
+                let { address } = await inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'address',
+                        message: 'Select Account',
+                        choices: [...addresses, new inquirer.Separator(), "Other"]
+                    }
+                ]);
+
+                if (address === 'Other') {
+                    address = await MenuUtil.askString("Enter address");
+                }
+
+                console.log(await leaser.getCurrentOpenLeasesByOwner(address));
             }
         }
     }
